0c2c57c781e44a51f76cf8f4699bae8bd11b6738
Alexander Luna <alexanderfluna@outlook.com>
2025-02-27 20:16:34 -0500

    Added to troubleshooting

--- src/relevant-information/NoPowerLight.jsx
+++ src/relevant-information/NoPowerLight.jsx
@@ -12,16 +12,16 @@ const NoPowerLight = () => {
       <button className="faq-question" onClick={() => toggleAnswer('no-power-light')}>The unit does not power on.</button>
       {visibleAnswer === 'no-power-light' && (
         <div className="faq-answer">
-          <p>1. Enter the device's part number into the <a href="https://acresecurity.com/secure-communications-networking-and-server-solutions/product-selector-tool">Product Selector Tool</a>.</p>
-          <p>2. Open the device's data sheet and locate its required input voltage (e.g. 8 - 24VDC, 48 - 56VDC).</p>
-          <p>3. Use a voltmeter to verify the power supply's polarity and voltage output.</p>
+          <p>[1] Enter the device's part number into the <a href="https://acresecurity.com/secure-communications-networking-and-server-solutions/product-selector-tool">Product Selector Tool</a>.</p>
+          <p>[2] Open the device's data sheet and locate its required input/operating voltage range (e.g. 8 - 24VDC, 48 - 56VDC).</p>
+          <p>[3] Use a voltmeter to verify the correct polarity and voltage while under load.</p>
           <li>Ensure the screws of the terminal block are securely tightened.</li>
-          <li>Confirm the red probe is connected to the voltmeter's positive terminal, the black probe is connected to the negative (or ground) terminal, and the voltmeter is set to DC voltage mode.</li>
-          <li>Touch the the red probe to the suspected positive terminal and the black probe to the suspected negative terminal.</li>
-          <li>A positive voltage reading indicates correct polarity; a negative reading indicates reversed polarity. If the polarity is reversed, swap the wires.</li>
-          <li>Verify the power supply is delivering the required input voltage.</li>
-          <p>4. Test that the power supply can power another device.</p>
-          <p>5. Switch to a different power source and repeat the steps above.</p>
+          <li>Confirm the red probe is connected to the voltmeter's positive terminal, the black probe is connected to the negative (or ground) terminal, and the voltmeter is set to DC voltage mode. </li>
+          <li>Touch the the red probe to the suspected positive terminal and the black probe to the suspected negative terminal. A positive voltage reading indicates correct polarity; a negative reading indicates reversed polarity.</li>
+          <li>If the polarity is reversed, swap the wires and retry.</li>
+          <p>[4] Determine if the power supply is delivering the required input/operating voltage.</p>
+          <li>If the voltage is less or greater than the required input/operating voltage range, replace the power supply and redo step 3.</li>
+          <li>If the voltage is within the required input/operating voltage range and the unit does not power on, contact technical support for an RMA.</li>
         </div>
       )}
     </div>
