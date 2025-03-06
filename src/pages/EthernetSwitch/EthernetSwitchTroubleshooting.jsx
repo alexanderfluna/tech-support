62d8d06ef99fc6c56cf9100965fce80dfc5bf6ba
alexanderfluna <alexanderfluna@outlook.com>
2025-03-06 12:46:50 -0500

    Updates

--- src/pages/EthernetSwitch/EthernetSwitchTroubleshooting.jsx
+++ src/pages/EthernetSwitch/EthernetSwitchTroubleshooting.jsx
@@ -78,14 +78,24 @@ function EthernetSwitchTroubleshooting() {
                         </button>
                         {visibleAnswers.has('no-communication') && (
                             <div className="faq-answer">
-                                <p>[1] Document which ports are affected.</p>
-                                <p>[2] Document if communication is restored automatically or if the switch requires a power cycle.</p>
-                                <p>[3] Document whether the switch locks up periodically (e.g. ever hour, every two hours, daily, etc.) or randomly.</p>
-                                <p>[4] Verify that the web interface is accessible.</p>
-                                <p>[5] Factory default the switch.</p>
-                                <p>[6] Identify the current firmware version of the switch and update to the latest version.</p>
-                                <p>[7] Isolate the switch outside of the network on a bench to determine if the issue persists.</p>
-                                <p>[8] Replace the switch with a known-working one to determine if the same issue persists with the new switch.</p>
+                                <p><strong>[1] Enter the device's part number into the <a href="https://acresecurity.com/secure-communications-networking-and-server-solutions/product-selector-tool">Product Selector Tool</a>.</strong></p>
+                                <p><strong>[2] Open the device's data sheet and locate its required input/operating voltage range (e.g. 8 - 24VDC, 48 - 56VDC).</strong></p>
+                                <p><strong>[3] Use a voltmeter to verify the correct polarity and voltage while under load.</strong></p>
+                                <p style={{paddingLeft: "40px"}}>[3.1] Ensure the screws of the terminal block are securely tightened.</p>
+                                <p style={{paddingLeft: "40px"}}>[3.2] Confirm the red probe is connected to the voltmeter's positive terminal, the black probe is connected to the negative (or ground) terminal, and the voltmeter is set to DC voltage mode. </p>
+                                <p style={{paddingLeft: "40px"}}>[3.3] Touch the the red probe to the suspected positive terminal and the black probe to the suspected negative terminal. A positive voltage reading indicates correct polarity; a negative reading indicates reversed polarity.</p>
+                                <p style={{paddingLeft: "40px"}}>[3.4] If the polarity is reversed, swap the wires and retry.</p>
+                                <p><strong>[4] Determine if the power supply is delivering the required input/operating voltage.</strong></p>
+                                <p style={{paddingLeft: "40px"}}>[4.1] If the voltage is less than the required input/operating voltage range, replace the power supply and redo step 3.</p>
+                                <p style={{paddingLeft: "40px"}}>[4.2] If the voltage is within the required input/operating voltage range and the unit does not power on, contact technical support.</p>
+                                <p>[5] Document which ports are affected.</p>
+                                <p>[6] Document if communication is restored automatically or if the switch requires a power cycle.</p>
+                                <p>[7] Document whether the switch locks up periodically (e.g. ever hour, every two hours, daily, etc.) or randomly.</p>
+                                <p>[8] Verify that the web interface is accessible.</p>
+                                <p>[9] Factory default the switch.</p>
+                                <p>[10] Identify the current firmware version of the switch and update to the latest version.</p>
+                                <p>[11] Isolate the switch outside of the network on a bench to determine if the issue persists.</p>
+                                <p>[12] Replace the switch with a known-working one to determine if the same issue persists with the new switch.</p>
                             </div>
                         )}
                     </div>
--- src/pages/MediaConverter/MediaConverterTroubleshooting.jsx
+++ src/pages/MediaConverter/MediaConverterTroubleshooting.jsx
@@ -70,8 +70,18 @@ function MediaConverterTroubleshooting() {
                         <button className="faq-question" onClick={() => toggleAnswer('no-communication')}> There is no Ethernet port activity. </button>
                         {visibleAnswers.has('no-communication') && (
                         <div className="faq-answer">
-                            <p>[1] Replace the Ethernet cable to determine if the issue persists.</p>
-                            <p>[2] Replace the media converter with the another of the same model to determine if the issue persists.</p>
+                            <p><strong>[1] Enter the device's part number into the <a href="https://acresecurity.com/secure-communications-networking-and-server-solutions/product-selector-tool">Product Selector Tool</a>.</strong></p>
+                            <p><strong>[2] Open the device's data sheet and locate its required input/operating voltage range (e.g. 8 - 24VDC, 48 - 56VDC).</strong></p>
+                            <p><strong>[3] Use a voltmeter to verify the correct polarity and voltage while under load.</strong></p>
+                            <p style={{paddingLeft: "40px"}}>[3.1] Ensure the screws of the terminal block are securely tightened.</p>
+                            <p style={{paddingLeft: "40px"}}>[3.2] Confirm the red probe is connected to the voltmeter's positive terminal, the black probe is connected to the negative (or ground) terminal, and the voltmeter is set to DC voltage mode. </p>
+                            <p style={{paddingLeft: "40px"}}>[3.3] Touch the the red probe to the suspected positive terminal and the black probe to the suspected negative terminal. A positive voltage reading indicates correct polarity; a negative reading indicates reversed polarity.</p>
+                            <p style={{paddingLeft: "40px"}}>[3.4] If the polarity is reversed, swap the wires and retry.</p>
+                            <p><strong>[4] Determine if the power supply is delivering the required input/operating voltage.</strong></p>
+                            <p style={{paddingLeft: "40px"}}>[4.1] If the voltage is less than the required input/operating voltage range, replace the power supply and redo step 3.</p>
+                            <p style={{paddingLeft: "40px"}}>[4.2] If the voltage is within the required input/operating voltage range and the unit does not power on, contact technical support.</p>
+                            <p>[5] Replace the Ethernet cable to determine if the issue persists.</p>
+                            <p>[6] Replace the media converter with the another of the same model to determine if the issue persists.</p>
                         </div>
                         )}
                     </div>
