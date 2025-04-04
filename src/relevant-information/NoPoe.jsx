75357b0fc8ecb903ccca97b81256bb0fe76974a1
Alexander Luna <aluna@ICSCOM.local>
2025-04-04 16:16:29 -0400

    Updates

--- src/relevant-information/NoPoe.jsx
+++ src/relevant-information/NoPoe.jsx
@@ -9,7 +9,7 @@ const NoPoE = () => {
       <p style={{paddingLeft: "40px"}}><strong>[2.2]</strong> Confirm that the screws of the terminal block on the unit and on the power supply are securely tighted.</p>
       <p style={{paddingLeft: "40px"}}><strong>[2.2]</strong> Confirm the red probe is connected to the voltmeter's positive terminal, the black probe is connected to the negative (or ground) terminal, and the voltmeter is set to DC voltage mode.</p>
       <p style={{paddingLeft: "40px"}}><strong>[2.3]</strong> Touch the red probe to the suspected positive terminal and the black probe to the suspected negative terminal. A positive voltage reading indicates correct polarity; a negative reading indicates reversed polarity.</p>
-      <p style={{paddingLeft: "40px"}}><strong>[2.4]</strong> If the voltage is less than 52VDC, increase the voltage. Otherwise, replace the power supply unit and recheck the voltage.</p>
+      <p style={{paddingLeft: "40px"}}><strong>[2.4]</strong> If the voltage is less than 52VDC, increase the voltage using the potentiometer. Otherwise, replace the power supply unit and recheck the voltage.</p>
       <p><strong>[3]</strong> Look up the data sheet of the PSE, power supply, and attached PDs.</p>
       <p style={{paddingLeft: "40px"}}><strong>[3.1]</strong> Ensure that the power supply rating (in watts) is sufficient to support the PSE and attached PDs.</p>
       <p style={{paddingLeft: "40px"}}><strong>[3.2]</strong> Verify that the PDs are compatible with the PoE standard of the PSE (802.3af, 802.3at, or 802.3bt).</p>
